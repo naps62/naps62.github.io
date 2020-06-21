7315f8a9acd977b0da0770f71d6e41f65f145bee
Miguel Palhas <mpalhas@gmail.com>
2020-06-21 17:11:23 +0100

    Slimmer posts

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,38 +1,51 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui"
-import { MDXRenderer } from "gatsby-plugin-mdx"
-import React from "react"
-import Layout from "./layout"
-import ItemTags from "./item-tags"
-import SEO from "./seo"
+import { jsx, Styled } from 'theme-ui';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import React from 'react';
+import Layout from './layout';
+import ItemTags from './item-tags';
+import SEO from './seo';
 
 type PostProps = {
   data: {
     post: {
-      slug: string
-      title: string
-      date: string
+      slug: string;
+      title: string;
+      date: string;
       tags?: {
-        name: string
-        slug: string
-      }[]
-      description?: string
-      body: string
-      excerpt: string
-      timeToRead: number
+        name: string;
+        slug: string;
+      }[];
+      description?: string;
+      body: string;
+      excerpt: string;
+      timeToRead: number;
       banner?: {
         childImageSharp: {
           resize: {
-            src: string
-          }
-        }
-      }
-    }
-  }
-}
+            src: string;
+          };
+        };
+      };
+    };
+  };
+};
 
-const px = [`32px`, `16px`, `8px`, `4px`]
-const shadow = px.map(v => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`)
+const px = [`32px`, `16px`, `8px`, `4px`];
+const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`);
+const postWidthCss = {
+  maxWidth: 650,
+  margin: 'auto',
+};
+
+const sectionCss = {
+  my: 5,
+  '.gatsby-resp-image-wrapper': {
+    my: [4, 4, 5],
+    boxShadow: shadow.join(`, `),
+  },
+  ...postWidthCss,
+};
 
 const Post = ({ data: { post } }: PostProps) => (
   <Layout>
@@ -41,8 +54,16 @@ const Post = ({ data: { post } }: PostProps) => (
       description={post.description ? post.description : post.excerpt}
       image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
     />
-    <Styled.h2>{post.title}</Styled.h2>
-    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+    <Styled.h2 cx={postWidthCss}>{post.title}</Styled.h2>
+    <p
+      sx={{
+        color: `secondary`,
+        mt: 3,
+        a: { color: `secondary` },
+        fontSize: [1, 1, 2],
+        ...postWidthCss,
+      }}
+    >
       <time>{post.date}</time>
       {post.tags && (
         <React.Fragment>
@@ -53,10 +74,10 @@ const Post = ({ data: { post } }: PostProps) => (
       {` — `}
       <span>{post.timeToRead} min read</span>
     </p>
-    <section sx={{ my: 5, ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) } }}>
+    <section sx={sectionCss}>
       <MDXRenderer>{post.body}</MDXRenderer>
     </section>
   </Layout>
-)
+);
 
-export default Post
+export default Post;
