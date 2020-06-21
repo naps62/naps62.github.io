ef953177a2af39d8959e6e7d3b61b8d86dbf28f9
Miguel Palhas <mpalhas@gmail.com>
2020-06-21 16:34:14 +0100

    More cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              showLineNumbers: false,
+              showLineNumbers: true,
             },
           },
         ],
